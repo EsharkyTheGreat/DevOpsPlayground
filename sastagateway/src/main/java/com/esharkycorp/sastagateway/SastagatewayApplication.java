3bb4cbd03340f7d6ad66be999978533694cbe586
EsharkyTheGreat <eshwars1210@gmail.com>
2024-08-31 23:01:45 +0530

    initialized sasta gateway

---
+++ sastagateway/src/main/java/com/esharkycorp/sastagateway/SastagatewayApplication.java
@@ -0,0 +1,13 @@
+package com.esharkycorp.sastagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SastagatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SastagatewayApplication.class, args);
+    }
+
+}
---
+++ sastagateway/src/test/java/com/esharkycorp/sastagateway/SastagatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esharkycorp.sastagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SastagatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
